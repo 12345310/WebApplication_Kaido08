5177d368e961b0e6a68472d8a5af5e54f46dd012
海藤 一臣 <qaswr.cdwef.8642@icloud.com>
2020-08-19 16:00:50 +0900

    First Commit

---
+++ WebApplication_Kaido08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Kaido08.MvcApplication" Language="C#" %>
---
+++ WebApplication_Kaido08/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Kaido08.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Importance { get; set; }
+        public DateTime Credit { get; set; }
+        public string Memo { get; set; }
+
+    }
+}
